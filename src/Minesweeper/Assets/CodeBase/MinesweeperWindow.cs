b80f0469e20dc5df5d3a20963144be8a7be63a87
scrawach <scrawach@gmail.com>
2024-08-27 16:04:22 +0300

    refactor minesweeaper window code

--- src/Minesweeper/Assets/CodeBase/MinesweeperWindow.cs
+++ src/Minesweeper/Assets/CodeBase/MinesweeperWindow.cs
@@ -22,29 +22,34 @@ namespace CodeBase
 
         public void Initialize(Board board)
         {
-            var cellSize = _grid.cellSize;
-            var cellPadding = _grid.padding;
-            var cellSpacing = _grid.spacing;
-            var sizeDelta = -1 * _gridRect.sizeDelta;
+            FitWindowForBoardWith(board.Width, board.Height);
 
-            var desiredBoardWidth = cellPadding.left + cellPadding.right 
-                                     + (cellSize.x * board.Width) 
-                                     + (cellSpacing.x * (board.Width - 1))
-                                     + sizeDelta.x;
-
-            var desiredBoardHeight = (cellPadding.top + cellPadding.bottom)
-                                     + (cellSize.y * board.Height)
-                                     + (cellSpacing.y * (board.Height - 1))
-                                     + sizeDelta.y;
-            
-            _window.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, desiredBoardWidth);
-            _window.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, desiredBoardHeight);
-            
             for (var y = 0; y < board.Height; y++)
             for (var x = 0; x < board.Width; x++)
             {
                 Instantiate(_cellPrefab, Vector3.zero, Quaternion.identity, _grid.transform);
             }
+        }
+
+        private void FitWindowForBoardWith(int width, int height)
+        {
+            var cellSize = _grid.cellSize;
+            var cellPadding = _grid.padding;
+            var cellSpacing = _grid.spacing;
+            var sizeDelta = _gridRect.sizeDelta;
+
+            var desiredBoardWidth = (cellPadding.left + cellPadding.right)
+                                    + cellSize.x * width
+                                    + cellSpacing.x * (width - 1)
+                                    - sizeDelta.x;
+
+            var desiredBoardHeight = cellPadding.top + cellPadding.bottom
+                                     + cellSize.y * height
+                                     + cellSpacing.y * (height - 1)
+                                     - sizeDelta.y;
+            
+            _window.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, desiredBoardWidth);
+            _window.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, desiredBoardHeight);
         }
     }
 }
