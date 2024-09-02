e37ff016d592f1f6ad35236501d6d8758d4c3a47
scrawach <scrawach@gmail.com>
2024-09-02 13:03:00 +0300

    moved code with filling board inside to board builder

--- src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
+++ src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
@@ -2,7 +2,15 @@ namespace CodeBase
 {
     public sealed class BoardBuilder
     {
-        public Board Build(int width, int height) => 
-            new(width, height);
+        public Board Build(int width, int height)
+        {
+            var board = new Board(width, height);
+            
+            for (var x = 0; x < width; x++)
+            for (var y = 0; y < height; y++)
+                board[x, y] = new Cell();
+
+            return board;
+        }
     }
 }
