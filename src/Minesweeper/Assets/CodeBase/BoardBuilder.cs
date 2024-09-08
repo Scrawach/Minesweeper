6c73967586a589c4c5f6d6899a108f0466552e42
scrawach <scrawach@gmail.com>
2024-09-08 17:53:30 +0300

    implemented naive mines generator inside board builder

--- src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
+++ src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
@@ -4,6 +4,8 @@ namespace CodeBase
 {
     public sealed class BoardBuilder
     {
+        private const int TargetAmountOfMines = 10;
+        
         public Board Build(int width, int height)
         {
             var board = new Board(width, height);
@@ -11,6 +13,25 @@ namespace CodeBase
             for (var x = 0; x < width; x++)
             for (var y = 0; y < height; y++)
                 board[x, y] = new Cell(new Vector2Int(x, y));
+
+            return GenerateMines(board);
+        }
+
+        public Board GenerateMines(Board board)
+        {
+            var mines = 0;
+
+            while (mines != TargetAmountOfMines)
+            {
+                var x = Random.Range(0, board.Width);
+                var y = Random.Range(0, board.Height);
+                
+                if (board[x, y].HasMine)
+                    continue;
+
+                board[x, y].HasMine = true;
+                mines++;
+            }
 
             return board;
         }
