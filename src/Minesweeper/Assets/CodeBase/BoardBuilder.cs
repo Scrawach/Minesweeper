5cff44e2ed0f96ff96b3272742492e52ff480df3
scrawach <scrawach@gmail.com>
2024-09-09 13:57:14 +0300

    added amount of mines to cell while mines generation

--- src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
+++ src/Minesweeper/Assets/CodeBase/BoardBuilder.cs
@@ -31,9 +31,22 @@ namespace CodeBase
 
                 board[x, y].HasMine = true;
                 mines++;
+                IncrementMinesAmountAround(board, x, y);
             }
 
             return board;
+        }
+
+        public void IncrementMinesAmountAround(Board board, int x, int y)
+        {
+            for (var i = x - 1; i < x + 2; i++)
+            for (var j = y - 1; j < y + 2; j++)
+            {
+                if (board.Contains(i, j) && !board[i,j].HasMine)
+                {
+                    board[i, j].AmountOfMinesAround++;
+                }
+            }
         }
     }
 }
